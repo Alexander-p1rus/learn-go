9afc258eab946bd197ebef23e9a0a5f311489f84
Alexander <alexanderulko20000@gmail.com>
2025-08-21 18:20:17 +0300

    refactoring | fix Read and Save funcs

--- 3-struct/main.go
+++ 3-struct/main.go
@@ -12,12 +12,19 @@ import (
 func main() {
 
 	bin, err := bins.СreateBin("222", true, time.Now(), "hello")
+	if err != nil {
+		fmt.Print(err.Error())
+	}
 
+	binsList, err := storage.Read("test.json")
+	fmt.Printf("binsListAFterRead: %v\n", binsList)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
+	binsList.AddBin(bin)
+	fmt.Printf("binsList: %v\n", binsList)
 
-	storage.SaveStorage("test.json", bin)
+	storage.Save("test.json", binsList)
 
 	file, err := files.ReadFile("test.json")
 	if err != nil {
--- 3-struct/storage/storage.go
+++ 3-struct/storage/storage.go
@@ -8,49 +8,32 @@ import (
 	"os-test/bins"
 )
 
-func SaveStorage(fileName string, b *bins.Bin) error {
-
-	file, err := os.Open(fileName)
-
-	if err != nil {
-		file, err = os.Create(fileName)
-		if err != nil {
-			return errors.New("не удалось создать файл")
-		}
-
-	}
-
-	binsList, err := ReadFile(fileName)
+func Save(fileName string, bl *bins.BinList) error {
+	err := os.WriteFile(fileName, bl.ToBytes(), 0644)
 	if err != nil {
 		return err
 	}
-
-	binsList.AddBin(b)
-
-	err = os.WriteFile(fileName, binsList.ToBytes(), 0644)
-	if err != nil {
-		return err
-	}
-
-	defer file.Close()
 
 	return nil
 }
 
-func ReadFile(fileName string) (*bins.BinList, error) {
+func Read(fileName string) (*bins.BinList, error) {
 	binsList := bins.BinList{Bins: []bins.Bin{}}
+
 	strBytes, err := os.ReadFile(fileName)
 
 	if err != nil {
-		return nil, errors.New("не удалось прочесть файл")
-	}
+		file, err := os.Create(fileName)
+		if err != nil {
+			return nil, err
+		}
 
-	if len(strBytes) == 0 {
+		file.Close()
+
 		return &binsList, nil
 	}
 
 	err = json.Unmarshal(strBytes, &binsList)
-
 	if err != nil {
 		return nil, errors.New("не удалость считать json файл")
 	}
